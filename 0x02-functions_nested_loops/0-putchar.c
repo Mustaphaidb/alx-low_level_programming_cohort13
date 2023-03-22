a2d76def2dab8d58b9b5ee70630a260f236d7218
Mustaphaidb <idbellam12@gmail.com>
2023-03-22 12:10:04 -0700

    TASK0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,21 @@
 #include "main.h"
 /**
  * main - Entry point
- *
- * Description: 'task 0'
- * @void: does not take any parameters
- *
- * Return: Always 0 (Success)
+ * Description: 'TASK0'
+ * Return: always 0
  */
 
 int main(void)
 {
-	char s[] = "_putchar";
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		_putchar(s[i]);
-	}
-	_putchar('\n');
+		_putchar('_');
+		_putchar('p');
+		_putchar('u');
+		_putchar('t');
+		_putchar('c');
+		_putchar('h');
+		_putchar('a');
+		_putchar('r');
+		_putchar('\n');
 	return (0);
 }
+
