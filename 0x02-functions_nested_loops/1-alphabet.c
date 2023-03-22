5f23fe9ae816e84d3d16a552e21cbb06199659b4
Mustaphaidb <idbellam12@gmail.com>
2023-03-22 12:41:49 -0700

    TASK1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_alphabet - print all alphabet in lowercase
- * 
- * Return: Always 0
+ * Print_alphabet - Print all alphabet in lowercase
+ * Description: 'TASK1' 
+ * Return: Always 0 (Success)
  */
 
 void print_alphabet(void)
