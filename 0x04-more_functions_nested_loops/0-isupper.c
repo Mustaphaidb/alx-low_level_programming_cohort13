1c386e9a0e3cfbf435c0b147017a266446da78e9
Mustaphaidb <idbellam12@gmail.com>
2023-03-23 19:06:34 -0700

    TASK0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+/**
+ * _isupper - check if uppercase wornjfwoieufnwoiuefnwieuf.
+ * Description: 'task 0'.
+ *
+ * @c: integer
+ *
+ * Return: Always 0 (Success)
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
