cf73d9c90639933e18c99857e66aa140b0adb26b
Mustaphaidb <idbellam12@gmail.com>
2023-03-29 19:59:14 -0700

    TASK1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  * swap_int - swap two values
+  * Description: Task1
+  * @a: value 1 - swap it to b
+  * @b: value 2 - swap it to a
+  * Return: 0
+*/
+void swap_int(int *a, int *b)
+{
+	int M;
+
+	M = *a;
+	*a = *b;
+	*b = M;
+
+
+
+}
