b83164292a1d5aa75a32abd38e2993d450282e53
Mustaphaidb <idbellam12@gmail.com>
2023-03-31 19:54:05 -0700

    TASK 0 to TASK 7

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,41 +1,38 @@
-#include "main.h"
-
-/**
- * _islower - checks if a character is lowercase
- * @c: character to check
- * Return: 1 if c is lowercase, 0 otherwise
- */
-static int _islower(int c)
-{
-	return (c >= 'a' && c <= 'z');
-}
-
-/**
- * _isseparator - checks if a character is a separator
- * @c: character to check
- * Return: 1 if c is a separator, 0 otherwise
- */
-static int _isseparator(char c)
-{
-	return (c == ' ' || c == '\t' || c == '\n' ||
-			c == ',' || c == ';' || c == '.' ||
-			c == '!' || c == '?' || c == '"' ||
-			c == '(' || c == ')' || c == '{' ||
-			c == '}');
-}
-
-/**
- * cap_string - capitalizes all words of a string
- * @s: string to capitalize
- * Return: pointer to string s
- */
-char *cap_string(char *s)
-{
-	int i;
-
-	for (i = 0; s[i]; ++i)
-		if (i == 0 || _isseparator(s[i - 1]))
-			(_islower(s[i])) ? s[i] -= 32 : 0;
-
-	return (s);
+#include "main.h"                                                                                                                     
+                                                                                                                                      
+/**                                                                                                                                   
+ * cap_string - Capitalizes all words of a string.                                                                                    
+ * @str: The string to be capitalized.                                                                                                
+ *                                                                                                                                    
+ * Return: A pointer to the changed string.                                                                                           
+ */                                                                                                                                   
+char *cap_string(char *str)                                                                                                           
+{                                                                                                                                     
+        int index = 0;                                                                                                                
+                                                                                                                                      
+        while (str[index])                                                                                                            
+        {                                                                                                                             
+                while (!(str[index] >= 'a' && str[index] <= 'z'))                                                                     
+                        index++;                                                                                                      
+                                                                                                                                      
+                if (str[index - 1] == ' ' ||                                                                                          
+                    str[index - 1] == '\t' ||                                                                                         
+                    str[index - 1] == '\n' ||                                                                                         
+                    str[index - 1] == ',' ||                                                                                          
+                    str[index - 1] == ';' ||                                                                                          
+                    str[index - 1] == '.' ||                                                                                          
+                    str[index - 1] == '!' ||                                                                                          
+                    str[index - 1] == '?' ||                                                                                          
+                    str[index - 1] == '"' ||                                                                                          
+                    str[index - 1] == '(' ||                                                                                          
+                    str[index - 1] == ')' ||                                                                                          
+                    str[index - 1] == '{' ||                                                                                          
+                    str[index - 1] == '}' ||                                                                                          
+                    index == 0)                                                                                                       
+                        str[index] -= 32;                                                                                             
+                                                                                                                                      
+                index++;                                                                                                              
+        }                                                                                                                             
+                                                                                                                                      
+        return (str);                                                                                                                 
 }
--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,22 +1,27 @@
-#include "main.h"
-
-/**
- * leet - encodes a string into 1337
- * @s: string to encode
- * Return: pointer to encoded string
- */
-char *leet(char *s)
-{
-	int i, j;
-	char *decoding = "aAeEoOtTlL";
-	char *encoding = "4433007711";
-
-	for (i = 0; s[i]; i++)
-	{
-		for (j = 0; decoding[j]; j++)
-			if (s[i] == decoding[j])
-				s[i] = encoding[j];
-	}
-
-	return (s);
+#include "main.h"                                                                                                                     
+                                                                                                                                      
+/**                                                                                                                                   
+ * leet - Encodes a string to 1337.                                                                                                   
+ * @str: The string to be encoded.                                                                                                    
+ *                                                                                                                                    
+ * Return: A pointer to the encoded string.                                                                                           
+ */                                                                                                                                   
+char *leet(char *str)                                                                                                                 
+{                                                                                                                                     
+        int indx1 = 0, indx2;                                                                                                         
+        char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};                                                                      
+                                                                                                                                      
+        while (str[indx1])                                                                                                            
+        {                                                                                                                             
+                for (indx2 = 0; indx2 <= 7; indx2++)                                                                                  
+                {                                                                                                                     
+                        if (str[indx1] == leet[indx2] ||                                                                              
+                            str[indx1] - 32 == leet[indx2])                                                                           
+                                str[indx1] = indx2 + '0';                                                                             
+                }                                                                                                                     
+                                                                                                                                      
+                indx1++;                                                                                                              
+        }                                                                                                                             
+                                                                                                                                      
+        return (str);                                                                                                                 
 }
