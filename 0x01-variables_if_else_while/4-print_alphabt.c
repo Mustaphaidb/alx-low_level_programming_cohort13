a9bc1c7ed826e32a846c5727418507163b51023b
Mustaphaidb <idbellam12@gmail.com>
2023-04-02 16:08:33 -0700

    TASK4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Description: 'Task.2'
- * @void: The main function takes no parameters
- * Return: Always 0 (Success)
+ * main - Entry Point
+ * 
+ * Description: Print alphabet except q and e using putchar 
+ * 
+ * Return: 0 (Success)
+ *
  */
-int main(void)
-{
+int main(void){
+
 	char c;
 
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		if (c == 'e' || c == 'q')
-			continue;
+	for (c = 'a'; c <= 'z'; c != q ; c++)
+		{
+		if ( c == 'q' || c == 'e')
+		continue;
 		putchar(c);
-	}
+		}
 	putchar('\n');
+
 	return (0);
 }
