1e92a02283fe49571252c900d728445de7c566ce
Mustaphaidb <idbellam12@gmail.com>
2023-04-02 18:19:25 -0700

    TASK6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
- * Description: 'Task'
+ * Description: 'Task 6'
  * @void: The main function takes no parameters
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	int n;
-
-	for (n = 48; n <= 57; n++)
-		putchar(n);
+	putchar('0');
+	putchar('1');
+	putchar('2');
+	putchar('3');
+	putchar('4');
+	putchar('5');
+	putchar('6');
+	putchar('7');
+	putchar('8');
+	putchar('9');
 	putchar('\n');
-
 	return (0);
 }
