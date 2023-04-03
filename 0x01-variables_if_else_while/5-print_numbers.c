9197c4cf68c1e1e376a043dc1b6e7c134c8d1b43
Mustaphaidb <idbellam12@gmail.com>
2023-04-02 17:36:18 -0700

    TASK5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,17 +8,13 @@
 
 int main(void)
 {
-	putchar(0);
-	putchar(1);
-	putchar(2);
-	putchar(3);
-	putchar(4);
-	putchar(5);
-	putchar(6);
-	putchar(7);
-	putchar(8);
-	putchar('9');
-	putchar('\n');
+	int n;
+	
+	for ( n = 0; n < 10; n++)
+		
+		printf("%d",n);
+
+		printf("\n");
 
 	return (0);
 }
