698a81a11d3d8c06c16d4d880293dbd87bef5e7d
Mustaphaidb <idbellam12@gmail.com>
2023-04-15 01:25:41 +0000

    TASK2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,22 +9,20 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-/* Declare a pointer to a 2D array of integers and increment variables*/
-	unsigned int i;
+unsigned int i;
 	char *memalloc;
-/* Check for invalid input */
+
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-/* Allocate memory for the array */
 	memalloc = malloc(nmemb * size);
-	if (memalloc == NULL) /* Check for allocation failure */
+	if (memalloc == NULL)
 		return (NULL);
 
-	for (i = 0; i < (nmemb * size); i++) /* Initialize each element of the array to zero */
+	for (i = 0; i < (nmemb * size); i++)
 	{
 		memalloc[i] = 0;
 	}
 
-return (memalloc); /*Return a pointer to the allocated memory of the array*/
+return (memalloc);
 }
