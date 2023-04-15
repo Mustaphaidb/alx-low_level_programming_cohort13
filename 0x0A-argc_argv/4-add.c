56880a4888952d42d42ba0805e504d5d82e811f3
Mustaphaidb <idbellam12@gmail.com>
2023-04-15 14:31:39 +0000

    TASK4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
+/**
+ * main - Starting point
+ * @argc: The number of command-line arguments
+ * @argv: An array of strings containing the command-line arguments
+ *
+ * Description: function that prints the sum of given numbers
+ * Return: On success 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum = 0, i;
+	bool isCorrect = true;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			char *p = argv[i];
+
+			while (*p)
+			{
+				if (!isdigit(*p))
+				{
+					isCorrect = false;
+					break;
+				}
+				p++;
+			}
+			if (!isCorrect)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			sum += atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+	}
+
+	return (0);
+}
